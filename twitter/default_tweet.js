202f46a02af38610e5a7959da0e33110edcc3d14
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-12 23:00:52 +0000

    Added a default tweet module

---
+++ twitter/default_tweet.js
@@ -0,0 +1,3 @@
+module.exports = function defaultTweet(screenName, deeplink) {
+    return `Hello @${screenName}. I'll be happy to check this for you. Please click the button below to start a conversation. ${deeplink}`
+};
