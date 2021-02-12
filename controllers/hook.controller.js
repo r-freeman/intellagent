5d878ee86c46cead9dd39c1edc7d3e3b88e69e96
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-12 22:53:06 +0000

    Updated hook controller

--- controllers/hook.controller.js
+++ controllers/hook.controller.js
@@ -1,25 +1,13 @@
-import tweetProcessor from '../twitter/tweet-processor';
-import messageProcessor from '../twitter/message-processor';
+import tweetProcessor from '../twitter/tweet_processor';
+import messageProcessor from '../twitter/message_processor';
 
 exports.post = (req, res) => {
     const event = req.body;
 
     if (event.tweet_create_events) {
-        // retrieve the first tweet in the array
-        const tweet = event.tweet_create_events.shift();
-
-        console.log(tweet);
+        tweetProcessor.process(event);
     } else if (event.direct_message_events) {
-        // grab the first message in the array
-        const message = event.direct_message_events.shift();
-
-        // get the sender id and screen name from the message
-        const sender = {
-            id: message.message_create.sender_id,
-            screenName: event.users[message.message_create.sender_id].screen_name
-        };
-
-        console.log(message);
+        messageProcessor.process(event);
     }
 
     res.send('OK');
