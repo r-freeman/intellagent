ace4050bb867bd4b23eae142fedcedc154883d1c
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-12 22:52:14 +0000

    Updated server.js to use port from env variables

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./routes/index.js');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
