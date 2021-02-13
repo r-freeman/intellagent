069c09233e223bcc9abddb451859b3e6f64762fb
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-13 00:45:05 +0000

    Added waving emoji to default tweet

--- twitter/default_tweet.js
+++ twitter/default_tweet.js
@@ -1,3 +1,3 @@
 module.exports = function defaultTweet(screenName, deeplink) {
-    return `Hello @${screenName}. I'll be happy to check this for you. Please click the button below to start a conversation. ${deeplink}`
+    return `Hello @${screenName} 👋 I'll be happy to check this for you. Please click the button below to start a conversation. ${deeplink}`
 };
