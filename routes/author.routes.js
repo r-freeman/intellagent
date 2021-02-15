2aa7fcf2c4242ffe72c0537d5a8544901afe6bb2
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-15 00:37:16 +0000

    Removed author model, controller, routes and middleware

--- routes/author.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const AuthorRoutes = express.Router();
-const AuthorController = require('../controllers/author.controller');
-
-const {validateAuthor} = require('../middleware/validators/author.validator');
-
-AuthorRoutes.post('/', validateAuthor, AuthorController.upsert);
-
-module.exports = AuthorRoutes;
