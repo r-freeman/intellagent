2be1218e53283c7a7165de50cedad6caa6e1e0bb
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-17 21:12:57 +0000

    Added category_responses module

---
+++ twitter/category_responses.js
@@ -0,0 +1,14 @@
+// maps classification categories to text responses
+module.exports = {
+    'ACCOUNT': 'this relates to an account problem.',
+    'INVOICES': 'this is an invoicing issue.',
+    'FEEDBACK': 'you would like to leave some feedback.',
+    'ORDER': 'the problem relates to an existing order.',
+    'REFUNDS': 'you are inquiring about a refund.',
+    'DELIVERY': 'you want to know the status of your delivery.',
+    'NEWSLETTER': 'you are looking for information about our newsletter.',
+    'SHIPPING': 'you have a question related to shipping.',
+    'CONTACT': 'you would like to speak to a human.',
+    'PAYMENT': 'this relates to payments.',
+    'CANCELLATION_FEE': 'you have question about the cancellation fee.'
+}
