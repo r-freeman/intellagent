bf4640face87e0e836107ad319359dfcc96fbab2
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-18 17:18:34 +0000

    Update validators to check for duplicate tweets

--- middleware/validators/tweet.validators.js
+++ middleware/validators/tweet.validators.js
@@ -31,31 +31,6 @@ exports.validateSchema = [
         .isEmpty()
         .withMessage('text is required.')
         .bail(),
-    // check('extended_tweet')
-    //     .trim()
-    //     .escape()
-    //     .optional({nullable: true}),
-    check('in_reply_to_user_id')
-        .trim()
-        .escape()
-        .not()
-        .isEmpty()
-        .withMessage('in_reply_to_user_id is required.')
-        .bail(),
-    check('in_reply_to_user_id_str')
-        .trim()
-        .escape()
-        .not()
-        .isEmpty()
-        .withMessage('in_reply_to_user_id_str is required.')
-        .bail(),
-    check('in_reply_to_screen_name')
-        .trim()
-        .escape()
-        .not()
-        .isEmpty()
-        .withMessage('in_reply_to_screen_name is required.')
-        .bail(),
     check('created_at')
         .trim()
         .escape()
@@ -82,42 +57,28 @@ exports.validateSchema = [
 ];
 
 // check for duplicate indexes
-// exports.validateCreate = [
-//     body('email').custom(email => {
-//         return Customer.findOne({email: email}).then(customer => {
-//             if (customer) {
-//                 return Promise.reject('email already exists');
-//             }
-//         });
-//     }),
-//     body('twitter_id').custom(twitter_id => {
-//         return Customer.findOne({twitter_id: twitter_id}).then(customer => {
-//             if (customer) {
-//                 return Promise.reject('twitter_id already exists');
-//             }
-//         });
-//     }),
-//     body('twitter_id_str').custom(twitter_id_str => {
-//         return Customer.findOne({twitter_id_str: twitter_id_str}).then(customer => {
-//             if (customer) {
-//                 return Promise.reject('twitter_id_str already exists');
-//             }
-//         });
-//     }),
-//     body('twitter_screen_name').custom(twitter_screen_name => {
-//         return Customer.findOne({twitter_screen_name: twitter_screen_name}).then(customer => {
-//             if (customer) {
-//                 return Promise.reject('twitter_screen_name already exists');
-//             }
-//         });
-//     }),
-//     (req, res, next) => {
-//         const errors = validationResult(req);
-//
-//         if (!errors.isEmpty()) {
-//             return res.status(422).json({errors: errors.array()});
-//         } else {
-//             next();
-//         }
-//     }
-// ];
+exports.validateCreate = [
+    body('tweet_id').custom(tweet_id => {
+        return Tweet.findOne({tweet_id: tweet_id}).then(tweet => {
+            if (tweet) {
+                return Promise.reject('tweet_id already exists');
+            }
+        });
+    }),
+    body('tweet_id_str').custom(tweet_id_str => {
+        return Tweet.findOne({tweet_id_str: tweet_id_str}).then(tweet => {
+            if (tweet) {
+                return Promise.reject('tweet_id_str already exists');
+            }
+        });
+    }),
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array()});
+        } else {
+            next();
+        }
+    }
+];
