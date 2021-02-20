56b2c5e9c2afa5c34c8bb726ba59a659839be889
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-20 22:48:46 +0000

    Added User model

---
+++ models/user.model.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: String
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Role',
+        required: true
+    }
+});
+
+// hash the users password on save or update
+UserSchema.pre('save', function (next) {
+    const user = this;
+
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+        if (err) return next(err);
+
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
