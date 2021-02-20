89910e64a232f72ae0bd53fed898aac14f7134e1
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-20 22:57:22 +0000

    Added users seeder

---
+++ seeders/2-users/users.js
@@ -0,0 +1,30 @@
+const faker = require('faker');
+const roles = require('../1-roles/roles');
+
+const [agent, manager, admin] = roles;
+const numAgents = 30;
+const numManagers = 5;
+const numAdmins = 1;
+
+const users = [];
+
+function createUsers(numUsers, role) {
+    for (let i = 0; i < numUsers; i++) {
+        let user = {};
+
+        user.name = faker.name.findName();
+        user.email = faker.internet.exampleEmail();
+        user.phone = faker.phone.phoneNumber();
+        user.password = 'secret';
+        user.role = role._id;
+
+        users.push(user);
+    }
+}
+
+// invoke createUsers for each role
+createUsers(numAgents, agent);
+createUsers(numManagers, manager);
+createUsers(numAdmins, admin);
+
+module.exports = users;
