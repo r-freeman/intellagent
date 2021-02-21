38aa56c22fdfd15876daebfdb202d86d67be95bf
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-21 22:00:22 +0000

    Updated user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,8 +3,7 @@ const Role = require('../models/role.model');
 
 exports.findAll = async (req, res) => {
     try {
-        let users = [];
-        users = await User.find().populate('role').select('name email phone role').exec();
+        let users = await User.find().populate('role').select('name email phone role').exec();
 
         return res.status(200).send(users);
     } catch (err) {
