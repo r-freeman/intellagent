398d193abc7bed9a85e86d0ebd4a03f7fb24c1e6
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-21 15:09:33 +0000

    Updated ticket controller and routes

--- controllers/ticket.controller.js
+++ controllers/ticket.controller.js
@@ -3,15 +3,17 @@ const Ticket = require('../models/ticket.model');
 // create a new ticket
 exports.create = async (req, res) => {
     try {
-        const {status} = req.body;
+        const {status, customer} = req.body;
 
         const ticket = await Ticket.create({
-            status
+            status,
+            customer
         });
 
         return res.status(201).send(ticket);
     } catch (err) {
         console.error(err);
+        return res.status(500).send();
     }
 };
 
@@ -19,9 +21,9 @@ exports.create = async (req, res) => {
 exports.findOne = async (req, res) => {
     try {
         const {_id, reference} = req.query;
-        const filter = typeof _id !== 'undefined' ? {_id: _id} : {reference: reference};
+        const filter = (typeof _id !== 'undefined' && _id.length === 24) ? {_id: _id} : {reference: reference};
 
-        const ticket = await Ticket.findOne(filter).exec();
+        const ticket = await Ticket.findOne(filter).populate('customer').exec();
 
         if (ticket !== null) {
             return res.status(200).send(ticket);
@@ -30,5 +32,18 @@ exports.findOne = async (req, res) => {
         }
     } catch (err) {
         console.error(err);
+        return res.status(500).send();
     }
 };
+
+// return all tickets
+exports.findAll = async (req, res) => {
+    try {
+        let ticket = await Ticket.find().populate('customer').exec();
+
+        return res.status(200).send(ticket);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send();
+    }
+}
--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const TicketRoutes = express.Router();
 const TicketController = require('../controllers/ticket.controller');
 
-// const {validateSchema, validateCreate} = require('../middleware/validators/tweet.validators');
+const {validateSchema, validateCreate} = require('../middleware/validators/ticket.validators');
 
-TicketRoutes.post('/', TicketController.create);
-TicketRoutes.get('/', TicketController.findOne);
+TicketRoutes.post('/new', validateSchema, validateCreate, TicketController.create);
+TicketRoutes.get('/show', TicketController.findOne);
+TicketRoutes.get('/list', TicketController.findAll);
 
 module.exports = TicketRoutes;
