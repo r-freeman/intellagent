ccac76e39baf62e1b6072263be1e47ce3aba07d2
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-21 22:29:28 +0000

    Added code to find team by id

--- controllers/team.controller.js
+++ controllers/team.controller.js
@@ -1,6 +1,25 @@
 const Team = require('../models/team.model');
 const Tag = require('../models/tag.model');
 
+exports.findOne = async (req, res) => {
+    try {
+        if (Object.keys(req.query).length !== 0) {
+            const {_id} = req.query;
+            const filter = (typeof _id !== 'undefined' && _id.length === 24) ? {_id: _id} : {};
+            const team = await Team.findOne(filter).populate('tags').exec();
+
+            if (team !== null) {
+                return res.status(200).send(team);
+            }
+        }
+
+        return res.status(404).send({});
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send();
+    }
+};
+
 exports.findAll = async (req, res) => {
     try {
         let teams = await Team.find().populate('tags').exec();
