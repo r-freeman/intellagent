07428cada97ff3aeb82d52f47f17c5752174422e
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-21 17:25:20 +0000

    Changed structure of tag seeder

--- seeders/3-tags/tags.js
+++ seeders/3-tags/tags.js
@@ -1,13 +1,61 @@
-const tagNames = [
-    'feedback', 'order', 'contact', 'payment', 'cancellation_fee',
-    'account', 'invoices', 'refunds', 'delivery', 'newsletter', 'shipping'
-];
+const bson = require('bson');
 
-const tags = tagNames.map(tag => {
-    return {
-        name: tag,
-        description: `This is the ${tag} tag`
-    }
-});
+const tags = [
+    {
+        _id: new bson.ObjectId("60318ca16eaa2755044b2a1b"),
+        name: 'feedback',
+        description: 'This is the feedback tag'
+    },
+    {
+        _id: new bson.ObjectId("60327a8b54d114303876bbe5"),
+        name: 'order',
+        description: 'This is the order tag'
+    },
+    {
+        _id: new bson.ObjectId("60327a8b54d114303876bbe6"),
+        name: 'contact',
+        description: 'This is the contact tag'
+    },
+    {
+        _id: new bson.ObjectId("60327a8b54d114303876bbe7"),
+        name: 'payment',
+        description: 'This is the payment tag'
+    },
+    {
+        _id: new bson.ObjectId("60327a8b54d114303876bbe8"),
+        name: 'cancellation_fee',
+        description: 'This is the cancellation fee tag'
+    },
+    {
+        _id: new bson.ObjectId("60327a8b54d114303876bbe9"),
+        name: 'account',
+        description: 'This is the account tag'
+    },
+    {
+        _id: new bson.ObjectId("60327a8b54d114303876bbea"),
+        name: 'invoices',
+        description: 'This is the invoices tag'
+    },
+    {
+        _id: new bson.ObjectId("60327a8b54d114303876bbeb"),
+        name: 'refunds',
+        description: 'This is the refunds tag'
+    },
+    {
+        _id: new bson.ObjectId("60327a8b54d114303876bbec"),
+        name: 'delivery',
+        description: 'This is the delivery tag'
+    },
+    {
+        _id: new bson.ObjectId("60327a8b54d114303876bbed"),
+        name: 'newsletter',
+        description: 'This is the newsletter tag'
+    },
+    {
+        _id: new bson.ObjectId("60327a8b54d114303876bbee"),
+        name: 'shipping',
+        description: 'This is the shipping tag'
+    },
+];
 
 module.exports = tags;
