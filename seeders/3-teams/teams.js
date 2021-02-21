1bc0643642d600f67f0f1775293f8e3d736976ea
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-21 23:25:45 +0000

    Added seeder to add users to teams

--- seeders/3-teams/teams.js
+++ seeders/3-teams/teams.js
@@ -17,16 +17,19 @@ const generalTagIds = filterTagIds(['account', 'feedback', 'newsletter', 'contac
 
 const teams = [
     {
+        _id: new bson.ObjectId('6032e937869b74468c21ffe0'),
         name: 'billing',
         description: 'This is the billing team',
         tags: billingTagIds
     },
     {
+        _id: new bson.ObjectId('6032e937869b74468c21ffe1'),
         name: 'fulfillment',
         description: 'This is the fulfillment team',
         tags: fulfillmentTagIds
     },
     {
+        _id: new bson.ObjectId('6032e937869b74468c21ffe2'),
         name: 'general',
         description: 'This is the general team',
         tags: generalTagIds
--- seeders/4-users/users.js
+++ seeders/4-users/users.js
@@ -1,30 +1,41 @@
-const bson = require('bson');
 const faker = require('faker');
 const roles = require('../1-roles/roles');
+const teams = require('../3-teams/teams');
 
 const [agent, manager, admin] = roles;
-const numAgents = 30;
+const [billingTeam, fulfillmentTeam, generalTeam] = teams;
+
+const numBillingAgents = 10;
+const numFulfillmentAgents = 10;
+const numGeneralAgents = 10;
 const numManagers = 5;
 const numAdmins = 1;
 
 const users = [];
 
-function createUsers(numUsers, role) {
+function createUsers(numUsers, role, team = null) {
     for (let i = 0; i < numUsers; i++) {
         let user = {};
 
         user.name = faker.name.findName();
         user.email = faker.internet.exampleEmail();
         user.phone = faker.phone.phoneNumber();
-        user.password = 'secret';
-        user.role = new bson.ObjectId(role._id);
+        user.password = '$2b$10$liAPhwoKUFg7ij55UwLdm.AUSCEfCMQME/oxtophd5n58shY0KNq2';
+        user.role = role._id;
+
+        if (team) {
+            user.team = team._id;
+        }
 
         users.push(user);
     }
 }
 
 // invoke createUsers for each role
-createUsers(numAgents, agent);
+createUsers(numBillingAgents, agent, billingTeam);
+createUsers(numFulfillmentAgents, agent, fulfillmentTeam);
+createUsers(numGeneralAgents, agent, generalTeam);
+
 createUsers(numManagers, manager);
 createUsers(numAdmins, admin);
 
