9ade9e4fd79b0ca9a3cff6c938c8f7a1f851b49e
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-21 22:32:04 +0000

    Added code to find user by id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,25 @@
 const User = require('../models/user.model');
 const Role = require('../models/role.model');
 
+exports.findOne = async (req, res) => {
+    try {
+        if (Object.keys(req.query).length !== 0) {
+            const {_id} = req.query;
+            const filter = (typeof _id !== 'undefined' && _id.length === 24) ? {_id: _id} : {};
+            const user = await User.findOne(filter).populate('role').exec();
+
+            if (user !== null) {
+                return res.status(200).send(user);
+            }
+        }
+
+        return res.status(404).send({});
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send();
+    }
+};
+
 exports.findAll = async (req, res) => {
     try {
         let users = await User.find().populate('role').select('name email phone role').exec();
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const UserRoutes = express.Router();
 const UserController = require('../controllers/user.controller');
 
+UserRoutes.get('/show', UserController.findOne);
 UserRoutes.get('/list', UserController.findAll);
 
 module.exports = UserRoutes;
