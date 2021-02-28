991ada000953dfd45fc94312a219d51fa8ed3467
Ryan Freeman <N00173767@student.iadt.ie>
2021-02-28 19:51:28 +0000

    Updated default port in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const webhooks = require('./twitter/webhooks');
 const routes = require('./routes/index.js');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
