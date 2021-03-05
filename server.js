56d28691752272a3e5901730f9c7325461be529f
Ryan Freeman <N00173767@student.iadt.ie>
2021-03-05 02:40:40 +0000

    Added jwt route guard middleware to protect certain routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const path = require('path');
 const express = require('express');
+const jwt = require('express-jwt');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 const mongooseConnect = require('./mongoose_connect');
 
 import passport from 'passport';
@@ -8,6 +10,8 @@ import passportUseStrategy from './passport';
 // const webhooks = require('./twitter/webhooks');
 
 passportUseStrategy(passport);
+
+const PUB_KEY = fs.readFileSync('id_rsa_pub.pem', 'utf8');
 
 const routes = require('./routes/index.js');
 
@@ -19,10 +23,22 @@ app.use(express.json());
 app.use(cors());
 app.use(passport.initialize());
 
-app.use('/api/v1', routes);
-
 // serve the front end from react build directory
 app.use(express.static(path.join(__dirname, './client/build')));
+
+// middleware for protecting all routes except auth routes
+app.use('/api/v1', jwt({
+    secret: PUB_KEY,
+    algorithms: ['RS256']
+}).unless({path: new RegExp('/auth/', 'i')}), routes);
+
+// custom error handler for jwt route guard
+app.use(function (err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).send({error: 'Unauthorised'});
+    }
+    // next();
+})
 
 // return not found error if route does not exist
 app.use(function (req, res, next) {
