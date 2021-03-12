8f717a8b30eed7b673c450f26f293162fc538b31
Ryan Freeman <N00173767@student.iadt.ie>
2021-03-12 13:13:00 +0000

    Updated testimonial section on Landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Nav from '../components/Nav';
 
 import dashboard from '../assets/images/dashboard.png';
-import apple from '../assets/svg/apple.svg';
 import grayHairedMan from '../assets/images/gray-haired-man.jpg';
 import transistor from '../assets/svg/transistor-logo-gray-400.svg';
 import mirage from '../assets/svg/mirage-logo-gray-400.svg';
@@ -162,15 +161,13 @@ function Landing() {
                                     <div
                                         className="absolute inset-0 bg-gradient-to-t from-blue-500 via-blue-500 opacity-80"/>
                                     <div className="relative px-8">
-                                        <div>
-                                            <img className="h-12"
-                                                 src={apple}
-                                                 alt="Apple"/>
+                                        <div className="h-12">
+
                                         </div>
                                         <blockquote className="mt-8">
                                             <div className="relative text-lg font-medium text-white md:flex-grow">
                                                 <svg
-                                                    className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-blue-300"
+                                                    className="absolute top-0 left-0 transform -translate-y-16 h-12 w-12 text-blue-400"
                                                     fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
                                                     <path
                                                         d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z"/>
@@ -181,10 +178,10 @@ function Landing() {
                                                     nulla feugiat dignissim id lobortis amet.
                                                 </p>
                                             </div>
-
-                                            <footer className="mt-4">
-                                                <p className="text-base font-semibold text-blue-200">Tim Cook,
-                                                    CEO at Apple</p>
+                                            <footer className="mt-4 inline-flex items-center">
+                                                <p className="text-base font-semibold text-blue-100 mr-2">Walter
+                                                    Armstrong,
+                                                    CEO</p>
                                             </footer>
                                         </blockquote>
                                     </div>
