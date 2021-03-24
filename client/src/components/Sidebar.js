1d502abd959dd82b8fb993d5d5f32fed4592b7f7
Ryan Freeman <N00173767@student.iadt.ie>
2021-03-24 14:50:38 +0000

    Updated Sidebar component

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,20 +1,23 @@
 import React, {useEffect} from 'react';
 import {Transition} from '@headlessui/react';
-import {NavLink} from 'react-router-dom';
+import {NavLink, useRouteMatch} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
 import actions from '../redux/actions';
 
 import logo from '../assets/svg/logo.svg';
 
 function Sidebar({sidebar, toggleSidebar}) {
-    const {teams, isFetchingTeams} = useSelector(state => state.teams);
+    const {teams} = useSelector(state => state.teams);
     const dispatch = useDispatch();
+    let match = useRouteMatch();
 
     useEffect(() => {
         if (teams.length === 0) {
             dispatch(actions.teams.fetchTeams());
         }
-    }, [dispatch]);
+    }, [dispatch, teams]);
+
+    const isCurrentPath = (path) => match.path.includes(path);
 
     return (
         <div>
@@ -40,7 +43,7 @@ function Sidebar({sidebar, toggleSidebar}) {
                         leave="transition ease-in-out duration-300 transform"
                         leaveFrom="translate-x-0"
                         leaveTo="-translate-x-full"
-                        className="relative flex-1 flex flex-col max-w-xs w-full py-4 bg-gray-100"
+                        className="relative flex-1 flex flex-col max-w-xs w-full py-4 bg-gray-50"
                     >
                         <div className="absolute top-0 right-0 -mr-12 pt-2">
                             <button
@@ -62,23 +65,31 @@ function Sidebar({sidebar, toggleSidebar}) {
                         <div className="mt-5 flex-1 h-0 overflow-y-auto">
                             <nav className="px-2">
                                 <div className="space-y-1">
-                                    {/*// <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:text-gray-900 hover:bg-gray-50" -->*/}
                                     <NavLink to="/dashboard"
-                                             className="bg-gray-200 text-gray-900 group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md"
-                                             aria-current="page">
-                                        {/*// <!-- Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500" -->*/}
-                                        <svg className="text-gray-500 mr-3 h-6 w-6"
-                                             xmlns="http://www.w3.org/2000/svg"
-                                             fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                                             aria-hidden="true">
+                                             className={`${isCurrentPath('dashboard')
+                                                 ? 'bg-gray-200 text-gray-900'
+                                                 : 'text-gray-600 hover:text-gray-900 hover:bg-gray-100'} group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md`}
+                                             aria-current="page"
+                                    >
+                                        <svg
+                                            className={`${isCurrentPath('dashboard')
+                                                ? 'text-gray-500'
+                                                : 'text-gray-400 group-hover:text-gray-500'} mr-3 h-6 w-6`}
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                                            aria-hidden="true">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                                   d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
                                         </svg>
                                         Dashboard
                                     </NavLink>
                                     <NavLink to="/tickets"
-                                             className="text-gray-600 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md">
-                                        <svg className="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
+                                             className={`${isCurrentPath('tickets')
+                                                 ? 'bg-gray-200 text-gray-900'
+                                                 : 'text-gray-600 hover:text-gray-900 hover:bg-gray-100'} group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md`}>
+                                        <svg className={`${isCurrentPath('tickets')
+                                            ? 'text-gray-500'
+                                            : 'text-gray-400 group-hover:text-gray-500'} mr-3 h-6 w-6`}
                                              xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                              stroke="currentColor" aria-hidden="true">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
@@ -86,8 +97,8 @@ function Sidebar({sidebar, toggleSidebar}) {
                                         </svg>
                                         Tickets
                                     </NavLink>
-                                    <NavLink to="/recent"
-                                             className="text-gray-600 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md">
+                                    <button type="button"
+                                            className="w-full text-gray-600 hover:text-gray-900 hover:bg-gray-100 group flex items-center px-2 py-2 text-base leading-5 font-medium rounded-md focus:outline-none">
                                         <svg className="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
                                              xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                              stroke="currentColor" aria-hidden="true">
@@ -95,7 +106,7 @@ function Sidebar({sidebar, toggleSidebar}) {
                                                   d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
                                         </svg>
                                         Recent
-                                    </NavLink>
+                                    </button>
                                 </div>
                                 <div className="mt-8">
                                     <h3 className="px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider"
@@ -105,15 +116,15 @@ function Sidebar({sidebar, toggleSidebar}) {
                                     <div className="mt-1 space-y-1" role="group" aria-labelledby="teams-headline">
                                         {teams.map(team => {
                                             return (
-                                                <NavLink to={`/teams/${team.name.toLowerCase()}`} key={team._id}
-                                                         className="group flex items-center px-3 py-2 text-base leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-50">
+                                                <button type="button" key={team._id}
+                                                        className="w-full group flex items-center px-3 py-2 text-base leading-5 font-medium text-gray-600 rounded-md hover:text-gray-900 hover:bg-gray-100 focus:outline-none">
                                                     <span
                                                         className={`bg-${team.colour}-500 w-2.5 h-2.5 mr-4 rounded-full`}
                                                         aria-hidden="true"/>
                                                     <span className="truncate">
                                                         {team.name}
                                                     </span>
-                                                </NavLink>
+                                                </button>
                                             )
                                         })}
                                     </div>
@@ -125,7 +136,7 @@ function Sidebar({sidebar, toggleSidebar}) {
                 </Transition>
             </div>
             <div className="hidden lg:flex lg:flex-shrink-0">
-                <div className="flex flex-col w-64 border-r border-gray-200 py-4 bg-gray-100 min-h-screen">
+                <div className="flex flex-col w-64 border-r border-gray-200 py-4 bg-gray-50 min-h-screen">
                     <div className="flex items-center flex-shrink-0 px-6">
                         <img className="h-10 w-auto"
                              src={logo}
@@ -134,29 +145,38 @@ function Sidebar({sidebar, toggleSidebar}) {
                     <div className="h-0 flex-1 flex flex-col overflow-y-auto">
                         <nav className="px-3 mt-5">
                             <div className="space-y-1">
-                                {/*// <!-- Current: "bg-gray-200 text-gray-900", Default: "text-gray-700 hover:text-gray-900 hover:bg-gray-50" -->*/}
                                 <NavLink to="/dashboard"
-                                         className="bg-gray-200 text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
-                                    {/*// <!-- Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500" -->*/}
-                                    <svg className="text-gray-500 mr-3 h-6 w-6" xmlns="http://www.w3.org/2000/svg"
-                                         fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                         className={`${isCurrentPath('dashboard')
+                                             ? 'bg-gray-200 text-gray-900'
+                                             : 'text-gray-700 hover:text-gray-900 hover:bg-gray-100'} group flex items-center px-2 py-2 text-sm font-medium rounded-md`}>
+                                    <svg
+                                        className={`${isCurrentPath('dashboard')
+                                            ? 'text-gray-500'
+                                            : 'text-gray-400 group-hover:text-gray-500 '} mr-3 h-6 w-6`}
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                               d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
                                     </svg>
                                     Dashboard
                                 </NavLink>
                                 <NavLink to="/tickets"
-                                         className="text-gray-700 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
-                                    <svg className="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
-                                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke="currentColor" aria-hidden="true">
+                                         className={`${isCurrentPath('tickets')
+                                             ? 'bg-gray-200 text-gray-900'
+                                             : 'text-gray-700 hover:text-gray-900 hover:bg-gray-100'} group flex items-center px-2 py-2 text-sm font-medium rounded-md`}>
+                                    <svg
+                                        className={`${isCurrentPath('tickets')
+                                            ? 'text-gray-500'
+                                            : 'text-gray-400 group-hover:text-gray-500 '} mr-3 h-6 w-6`}
+                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke="currentColor" aria-hidden="true">
                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                               d="M4 6h16M4 10h16M4 14h16M4 18h16"/>
                                     </svg>
                                     Tickets
                                 </NavLink>
-                                <NavLink to="/recent"
-                                         className="text-gray-700 hover:text-gray-900 hover:bg-gray-50 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
+                                <button type="button"
+                                        className="w-full text-gray-700 hover:text-gray-900 hover:bg-gray-100 group flex items-center px-2 py-2 text-sm font-medium rounded-md focus:outline-none">
                                     <svg className="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
                                          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                          stroke="currentColor" aria-hidden="true">
@@ -164,7 +184,7 @@ function Sidebar({sidebar, toggleSidebar}) {
                                               d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
                                     </svg>
                                     Recent
-                                </NavLink>
+                                </button>
                             </div>
                             <div className="mt-8">
                                 <h3 className="px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider"
@@ -174,15 +194,15 @@ function Sidebar({sidebar, toggleSidebar}) {
                                 <div className="mt-1 space-y-1" role="group" aria-labelledby="teams-headline">
                                     {teams.map(team => {
                                         return (
-                                            <NavLink to={`/teams/${team.name.toLowerCase()}`} key={team._id}
-                                                     className="group flex items-center px-3 py-2 text-sm font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50">
+                                            <button type="button" key={team._id}
+                                                    className="w-full group flex items-center px-3 py-2 text-sm font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-100 focus:outline-none">
                                                 <span
                                                     className={`bg-${team.colour}-500 w-2.5 h-2.5 mr-4 rounded-full`}
                                                     aria-hidden="true"/>
                                                 <span className="truncate">
                                                     {team.name}
                                                 </span>
-                                            </NavLink>
+                                            </button>
                                         )
                                     })}
                                 </div>
