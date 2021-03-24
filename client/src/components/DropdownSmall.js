5ff33512c34c3634d0999f856ad63c7d4df5b9ca
Ryan Freeman <N00173767@student.iadt.ie>
2021-03-24 18:14:08 +0000

    Added notification button to DropdownSmall component

--- client/src/components/DropdownSmall.js
+++ client/src/components/DropdownSmall.js
@@ -30,7 +30,23 @@ function DropdownSmall({dropdown, toggleSidebar, toggleDropdown, setDropdown}) {
                     <div className="w-full flex md:ml-0"/>
                 </div>
                 <div className="flex items-center">
-                    <div className="ml-3 relative">
+                    <div className="relative inline-flex mr-4">
+                        <button type="button"
+                                className="inline-flex items-center text-gray-400 hover:text-gray-500 rounded-md transition ease-in-out duration-150 focus:outline-none">
+                            <svg className="w-6 h-6" fill="none" stroke="currentColor"
+                                 viewBox="0 0 24 24"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                      d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
+                            </svg>
+                        </button>
+                        <span className="flex absolute h-3 w-3 top-0 right-0 -mt-1 -mr-1">
+                            <span
+                                className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"/>
+                            <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"/>
+                        </span>
+                    </div>
+                    <div className="relative">
                         <div>
                             <button
                                 onBlur={() => setDropdown(false)}
