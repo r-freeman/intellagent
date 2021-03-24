5c10a8728fcac4dac6af5e2c288f44e52e36b782
Ryan Freeman <N00173767@student.iadt.ie>
2021-03-24 14:49:37 +0000

    Updated DropdownLarge component

--- client/src/components/DropdownLarge.js
+++ client/src/components/DropdownLarge.js
@@ -74,7 +74,7 @@ function DropdownLarge({dropdown, setDropdown, toggleDropdown}) {
                         <button type="button"
                                 className="block w-full text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                                 role="menuitem"
-                                onClick={handleLogout}>Logout
+                                onClick={handleLogout}>Log out
                         </button>
                     </div>
                 </div>
