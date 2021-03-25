a535716e9f44bec1503e06130ecd4c7bb1f1886e
Ryan Freeman <N00173767@student.iadt.ie>
2021-03-25 14:41:58 +0000

    Update Ticket model

--- models/ticket.model.js
+++ models/ticket.model.js
@@ -4,9 +4,13 @@ const mongoose = require('mongoose');
 const nanoId = customAlphabet('0123456789', 8);
 
 const TicketSchema = new mongoose.Schema({
+    reference: {
+        type: String,
+        unique: true
+    },
     status: {
         type: String,
-        enum: ['unassigned', 'open', 'closed'],
+        enum: ['unassigned', 'open', 'closed', 'hidden'],
         default: 'unassigned'
     },
     user: {
@@ -20,13 +24,31 @@ const TicketSchema = new mongoose.Schema({
         ref: 'Customer',
         required: true
     },
-    reference: {
-        type: String,
-        unique: true
-    },
     issue_type: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Tag'
+    },
+    messages: [
+        {
+            incoming: {
+                type: Boolean,
+                required: true
+            },
+            read: {
+                type: Boolean,
+                required: false,
+                default: false
+            },
+            body: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    welcome_message_id: {
+        type: String,
+        required: false,
+        unique: true
     }
 }, {timestamps: true});
 
@@ -36,8 +58,14 @@ TicketSchema.pre('save', function (next) {
     next();
 });
 
+// locate ticket by referenece number
 TicketSchema.statics.findByReference = async function (reference) {
     return await this.findOne({reference}).exec();
+};
+
+// locate ticket by welcome message id
+TicketSchema.statics.findByWelcomeMessageId = async function (welcome_message_id) {
+    return await this.findOne({welcome_message_id}).exec();
 };
 
 const Ticket = mongoose.model('Ticket', TicketSchema);
